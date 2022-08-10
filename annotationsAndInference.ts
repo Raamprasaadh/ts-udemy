5a252a796860205ee1affcfc7cb0152c4d99b15f
raamprasaadh <ramprasadhsurya@gmail.com>
2022-08-10 22:29:11 +0530

    explains types annotations & inference

---
+++ annotationsAndInference.ts
@@ -0,0 +1,5 @@
+/**
+ * type annotations and type inferences behave differently for variables, functions and objects.
+ * type annotation is when we explicitly mention what type of value a variable will have
+ * type inference is when the compiler tries to figure out what type of value a variable refers to
+ */
